84e75865989c29e24c50b104bbf62318f597c67d
Mariana Meyer <marianacmeyer@gmail.com>
2024-08-16 13:13:08 -0300

    adicionado os arquivos index.html, index.js, imagens/ e src/

---
+++ index.js
@@ -0,0 +1,178 @@
+// document.addEventListener('DOMContentLoaded',() =>{
+//     const texto =
+//     document.getElementById('perguntaOculta1');
+    
+//     const botao =
+//     document.getElementById('faq-botao1');
+    
+//     faq-botao1.addEventListener('click', () =>{
+//         if(texto.style.display === 'none'){
+//             texto.style.display = 'block';
+//             botao.textContent = 'Ocultar Texto';{
+//                 else{
+//                     texto.style.display = 'none';
+//                     botao.textContent = 'Mostrar Texto';
+//                 }
+//             }}});
+//         });
+
+//------------------------------------------------------------------------
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+//-------------------------------------------areas de atuação---------------
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+//----------------------------------avaliaçao neuropsicologica-------------------
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+// -----------------------------------faq--------------------------------------
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
+
+
+function toggle(el) {
+    var display = document.getElementById(el).style.display;
+    if(display == "none") {
+        document.getElementById(el).style.display = 'block';
+    }
+    else{
+        document.getElementById(el).style.display = 'none';
+    }
+}
